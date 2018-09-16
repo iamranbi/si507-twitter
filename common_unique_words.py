17ea47ff6dc593e742188671b018d12dc80eb8b5
iamranbi <ranbi0506@gmail.com>
2018-09-16 01:06:31 -0400

    first commit

---
+++ common_unique_words.py
@@ -0,0 +1,133 @@
+from requests_oauthlib import OAuth1
+import json
+import sys
+import requests
+import secret_data # file that contains OAuth credentials
+import nltk
+
+## if the package punkt is not installed on your computer, please uncomment the following section of code.
+#import ssl
+#try:
+    #_create_unverified_https_context = ssl._create_unverified_context
+#except AttributeError:
+    #pass
+#else:
+    #ssl._create_default_https_context = _create_unverified_https_context
+#nltk.download('punkt')
+
+
+##SI 507 hw5_twitter_ec1
+## Name: Ran Bi
+## Your section day/time: 004 Monday 7-8:30p
+## Any names of people you worked with on this assignment: worked alone
+##usage should be python3 hw5_twitter_ec1.py <username 1> <username 2> <num_tweets>
+
+##Step 1: get tweets
+#1.1 input
+username1 = sys.argv[1]
+username2 = sys.argv[2]
+num_tweets = sys.argv[3] if len(sys.argv) >= 4 else 20
+consumer_key = secret_data.CONSUMER_KEY
+consumer_secret = secret_data.CONSUMER_SECRET
+access_token = secret_data.ACCESS_KEY
+access_secret = secret_data.ACCESS_SECRET
+
+#1.2 OAuth starts
+url = 'https://api.twitter.com/1.1/account/verify_credentials.json'
+auth = OAuth1(consumer_key, consumer_secret, access_token, access_secret)
+requests.get(url, auth=auth)
+
+#1.3 get tweets from api
+baseurl = 'https://api.twitter.com/1.1/statuses/user_timeline.json'
+params1 = {'screen_name':username1, 'count':num_tweets}
+params2 = {'screen_name':username2, 'count':num_tweets}
+resp1 = requests.get(baseurl, params1, auth=auth)
+resp2 = requests.get(baseurl, params2, auth=auth)
+r1_json=json.loads(resp1.text)
+r2_json=json.loads(resp2.text)
+
+#1.4 gather tweet text
+tokenized_list1=[]
+tokenized_list2=[]
+for i in r1_json:
+    text=i['text']
+    n_text=nltk.word_tokenize(text)
+    tokenized_list1=tokenized_list1+n_text
+
+for i in r2_json:
+    text=i['text']
+    n_text=nltk.word_tokenize(text)
+    tokenized_list2=tokenized_list2+n_text
+
+
+##Step 2: analyze most frequent common words
+#2.1 function that ignores stop words
+def ignore_word(list_of_words):
+    word_keep = []
+    for word in list_of_words:
+        if (word not in ['','http','RT','https'] and word[0].isalpha()):
+            word_keep.append(word)
+    return word_keep
+
+#2.2 function that gets frequency distribution
+def freq_dict(l):
+    d=dict()
+    for i in l:
+        d[i]=d.get(i, 0) + 1
+    return d
+
+#2.3 ignore stop words and get frequency distribution
+tokenized_list_ignored1=ignore_word(tokenized_list1)
+tokenized_list_ignored2=ignore_word(tokenized_list2)
+freq_word1=freq_dict(tokenized_list_ignored1)
+freq_word2=freq_dict(tokenized_list_ignored2)
+
+#2.4 get common words
+common_keys=set(freq_word1).intersection(freq_word2)
+common=dict()
+common_five=[]
+for key in common_keys:
+    common[key]=min(freq_word1[key],freq_word2[key])
+
+common_sorted=sorted(common.items(), key=lambda kv: kv[1], reverse=True)
+for i in common_sorted[0:5]:
+    ii='{}({})'.format(i[0],i[1])
+    common_five.append(ii)
+
+common_five_str=' '.join(common_five)
+
+
+##Step 3: analyze most frequent different words
+#3.1 removes words shared by two users
+def common_key_remove(common_keys, the_dict):
+    for key in common_keys:
+        if key in the_dict:
+            del the_dict[key]
+
+common_key_remove(common_keys,freq_word1)
+common_key_remove(common_keys,freq_word2)
+freq_word1_sorted=sorted(freq_word1.items(), key=lambda kv: kv[1], reverse=True)
+freq_word2_sorted=sorted(freq_word2.items(), key=lambda kv: kv[1], reverse=True)
+
+#3.2 get frequent diffent words
+user1_diff_five=[]
+user2_diff_five=[]
+for i in freq_word1_sorted[0:5]:
+    ii='{}({})'.format(i[0],i[1])
+    user1_diff_five.append(ii)
+
+for i in freq_word2_sorted[0:5]:
+    ii='{}({})'.format(i[0],i[1])
+    user2_diff_five.append(ii)
+
+user1_diff_five_str=' '.join(user1_diff_five)
+user2_diff_five_str=' '.join(user2_diff_five)
+
+
+#Step 4: output
+print('USER 1:', username1)
+print('USER 2:', username2)
+print('TWEETS ANALYZED:', min(len(r1_json),len(r2_json)))
+print('5 MOST FREQUENT DIFFERENT WORDS FOR USER 1:', user1_diff_five_str)
+print('5 MOST FREQUENT DIFFERENT WORDS FOR USER 2:', user2_diff_five_str)
+print('5 MOST FREQUENT COMMON WORDS:', common_five_str)
---
+++ frequently_used_words.py
@@ -0,0 +1,121 @@
+from requests_oauthlib import OAuth1
+import json
+import sys
+import requests
+import secret_data # file that contains OAuth credentials
+import nltk
+
+## if the package punkt is not installed on your computer, please uncomment the following section of code.
+#import ssl
+#try:
+    #_create_unverified_https_context = ssl._create_unverified_context
+#except AttributeError:
+    #pass
+#else:
+    #ssl._create_default_https_context = _create_unverified_https_context
+#nltk.download('punkt')
+
+## SI 507 - HW5
+## COMMENT WITH:
+## Name: Ran Bi
+## Your section day/time: 004 Monday 7-8:30p
+## Any names of people you worked with on this assignment: worked alone
+
+#usage should be python3 hw5_twitter.py <username> <num_tweets>
+username = sys.argv[1]
+num_tweets = sys.argv[2]
+
+consumer_key = secret_data.CONSUMER_KEY
+consumer_secret = secret_data.CONSUMER_SECRET
+access_token = secret_data.ACCESS_KEY
+access_secret = secret_data.ACCESS_SECRET
+
+#Code for OAuth starts
+url = 'https://api.twitter.com/1.1/account/verify_credentials.json'
+auth = OAuth1(consumer_key, consumer_secret, access_token, access_secret)
+requests.get(url, auth=auth)
+#Code for OAuth ends
+
+#Write your code below:
+#Code for Part 3:Caching
+##try to load the cache from file
+CACHE_FNAME = 'twitter_cache.json'
+try:
+    cache_file = open(CACHE_FNAME, 'r')
+    cache_contents = cache_file.read()
+    CACHE_DICTION = json.loads(cache_contents)
+    cache_file.close()
+except:
+    CACHE_DICTION = {}
+##return a string that uniquely represents the fullURL of the request
+def params_unique_combination(baseurl, params):
+    alphabetized_keys = sorted(params.keys(),reverse=True)
+    res = []
+    for k in alphabetized_keys:
+        res.append('{}={}'.format(k, params[k]))
+    return baseurl + '?'+ "&".join(res)
+
+def make_request_using_cache(baseurl, params):
+    unique_ident = params_unique_combination(baseurl,params)
+    ##look in the cache to see if already have this data
+    if unique_ident in CACHE_DICTION:
+        print('Fetching cached data...')
+        print('----------------')
+        return CACHE_DICTION[unique_ident]
+    ##if not, fetch the data afresh, add it to the cache, write the cache to file
+    else:
+        print('Making request for new data...')
+        print('----------------')
+        resp = requests.get(baseurl, params, auth=auth)
+        CACHE_DICTION[unique_ident] = json.loads(resp.text)
+        dumped_json_cache = json.dumps(CACHE_DICTION)
+        fw = open(CACHE_FNAME,"w")
+        fw.write(dumped_json_cache)
+        fw.close()
+        return CACHE_DICTION[unique_ident]
+
+#Code for Part 1:Get Tweets
+baseurl = 'https://api.twitter.com/1.1/statuses/user_timeline.json'
+params = {'screen_name':username, 'count':num_tweets}
+##fetch tweets
+r_json=make_request_using_cache(baseurl, params)
+##write the json returned
+#with open('tweet.json', 'w') as f:
+    #json.dump(r_json,f,indent=2)
+
+#Code for Part 2:Analyze Tweets
+def ignore_word(list_of_words):
+    word_keep = []
+    for word in list_of_words:
+        if (word not in ['','http','RT','https'] and word[0].isalpha()):
+            word_keep.append(word)
+    return word_keep
+
+tokenized_list=[]
+freq_word_five=[]
+##gather tweet data from the response of twitter api
+for i in r_json:
+    text=i['text']
+    n_text=nltk.word_tokenize(text)
+    tokenized_list=tokenized_list+n_text
+##ignore stop words
+tokenized_list_ignored=ignore_word(tokenized_list)
+##get a frequency distribution of the tokenized list
+freq_word=nltk.FreqDist(tokenized_list_ignored)
+##5 most frequently used words
+for i in freq_word.most_common(5):
+    ii='{}({})'.format(i[0],i[1])
+    freq_word_five.append(ii)
+freq_word_str=' '.join(freq_word_five)
+print('USER:', username)
+print('TWEETS ANALYZED:', len(r_json))
+print('5 MOST FREQUENT WORDS:', freq_word_str)
+
+
+if __name__ == "__main__":
+    if not consumer_key or not consumer_secret:
+        print("You need to fill in client_key and client_secret in the secret_data.py file.")
+        exit()
+    if not access_token or not access_secret:
+        print("You need to fill in this API's specific OAuth URLs in this file.")
+        exit()
